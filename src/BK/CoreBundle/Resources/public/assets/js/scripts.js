065c891491c9ef1287355c713d3f68e37c0b3156
Quan MT <info@brodev.com>
2014-09-04 12:00:31 +0700

    Update new content

--- src/BK/CoreBundle/Resources/public/assets/js/scripts.js
+++ src/BK/CoreBundle/Resources/public/assets/js/scripts.js
@@ -33,42 +33,7 @@
 			app.fn.screenSize();
 		});		
       
-	//Flexislider for testimonials
-	if($('.testimonials-slider').length != 0) {
-		$('.testimonials-slider').flexslider({
-			manualControls: '.flex-manual .switch',
-			nextText: "",
-			prevText: "",
-			startAt: 1,
-			slideshow: false,
-			direction: "horizontal",
-			animation: "slide"
-		});
-	};
-    
-    // Headhesive init
-    var options = {  // set options
-            offset: '#showHere',
-            classes: {
-                clone:   'fixmenu-clone',
-                stick:   'fixmenu-stick',
-                unstick: 'fixmenu-unstick'
-            }
-        };
-	
-	if($('#registration').length == 0) {
-		var fixmenu = new Headhesive('.navigation-header', options); // init
-	}
-	
-    // Navigation Scroll
-    $('.navigation-bar').onePageNav({
-        currentClass: 'active',
-        changeHash: true,
-        scrollSpeed: 750,
-        scrollThreshold: 0.5,
-        easing: 'swing'
-    });
-    
+
     // Animated Appear Element
 	if (app.el['window'].width() > 1024){
 		
@@ -110,66 +75,16 @@
             easing: 'swing'
         }, 750);
         return false;
-    });   
-
-    // count down timer
-    var futureDate = new Date();
-    // count down 10 days from today
-    futureDate.setDate( futureDate.getDate() + 10 );    
-    // or set specific date in the future
-    // futureDate = new Date(2014, 7, 26);
-    $('.countdown').countdown({
-        until       : futureDate, 
-        compact     : true, 
-        padZeroes   : true,
-        layout      : $('.countdown').html()
     });
 
-    // Form validation - sign up
-	toastr.options = {"positionClass": "toast-top-full-width"};
-    if( $('.form-register').length ) {
-      $('.form-register').validate({
-        rules: {
-          password: {
-            minlength: 5
-          },
-          confirmPassword: {
-            minlength: 5,
-            equalTo: "#password"
-          }
-        },
-        messages: {
-          fullname: "Please enter your fullname",
-          password: {
-            required: "Please provide a password",
-            minlength: "Your password must be at least 5 characters long"
-          },
-          confirmPassword: {
-            required: "Please provide a password",
-            minlength: "Your password must be at least 5 characters long",
-            equalTo: "Please enter the same password as above"
-          },
-          email: "Please enter a valid email address"
-        },      
-        submitHandler: function(form) {
-          var $this = $(form);
-          $.ajax({
-            url: $this.attr('action'),
-            type: 'POST',
-            data: $this.serialize(),
-          })
-          .done(function(msg) {
-            if( msg == 'ok' ) {
-              toastr.success('Thank you for signing up.');
-              $this[0].reset();
-            } else {
-              toastr.error('An error occured. Please try again later.');
-            }
-          })
-          .fail(function() {
-            toastr.error('An error occured. Please try again later.');
-          });
-        }      
-      });
-    }
+    $('.to-sign-up-form').click(function(e) {
+        $('html, body').animate({
+            scrollTop: $("#sign-up").offset().top,
+            easing: 'swing'
+        }, 300, function() {
+            $('#email-input').focus();
+        });
+
+        e.preventDefault();
+    });
 })();
