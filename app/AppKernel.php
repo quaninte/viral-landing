920d732a3ce08197c7a8a7b93a481fc092791b6a
Minh Tran <minhtran@brodev.com>
2014-10-17 15:22:23 +0700

    Add jsrouting bundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -20,7 +20,8 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new BK\CoreBundle\BKCoreBundle(),
             new Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle(),
-            new Kachkaev\AssetsVersionBundle\KachkaevAssetsVersionBundle()
+            new Kachkaev\AssetsVersionBundle\KachkaevAssetsVersionBundle(),
+            new FOS\JsRoutingBundle\FOSJsRoutingBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
